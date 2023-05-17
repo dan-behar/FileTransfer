522b73c2c85bb7bb3d14a6fc1803f18c720a5272
Daniel Behar <70411889+dan-behar@users.noreply.github.com>
2023-05-17 13:02:21 -0600

    Update server.js to accept css

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const express = require("express")
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'));
 
 const upload = multer({ dest: "uploads" })
 
